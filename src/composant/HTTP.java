81b4c67c9daa741c1907ff349bd8034fc042ae4f
dreamlumos <30956230+dreamlumos@users.noreply.github.com>
2020-12-21 16:04:48 +0100

    HTTP

--- src/composant/HTTP.java
+++ src/composant/HTTP.java
@@ -2,13 +2,29 @@ package composant;
 
 public class HTTP {
 
+	private String http;
+	
+	/* http must be a String of bytes separated by spaces */
+	public HTTP(String http) {
+		this.http = http;
+		// check if HTTP?
+	}
 	
 	@Override
 	public String toString() {
-		
+				
 		StringBuilder sb = new StringBuilder();
+		sb.append("Hypertext Transfer Protocol\n\t");
 		
-		
+		for (int i = 0; i < http.length(); i += 3) {
+			String oneByte = http.substring(i, i+2);
+			char ch = (char)Integer.parseInt(oneByte, 16);
+		    sb.append(ch);
+		    
+		    if (ch == '\n') {
+		    	sb.append("\t");
+		    }
+		}
 		
 		return sb.toString();
 	}
